c2cd535640c15f9fff2b1009ef46bbe3036af185
bear <csxxxiong@gmail.com>
2019-07-16 18:45:00 +0800

    【优化】相册部分工具
    部分相册部分工具

---
+++ xPort/src/main/java/me/csxiong/library/utils/album/OnResultListener.java
@@ -0,0 +1,14 @@
+package me.csxiong.library.utils.album;
+
+import java.util.List;
+
+/*********************************************
+ * Version : 10:04 AM
+ * Author  : csxiong - 2019/5/17
+ * Changes : result callback for XAlbum
+ *********************************************
+ */
+public interface OnResultListener {
+
+    void onResult(List<ImageEntity> imageEntities);
+}
