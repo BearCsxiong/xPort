24e15ba65a87a09298baa2c34e162edc64380972
csxiong <494065558@qq.com>
2019-07-17 01:44:05 +0800

    【优化】SW屏幕适配方案适配负dp

--- AndroidSWGenerator.java
+++ AndroidSWGenerator.java
@@ -1,7 +1,3 @@
-/**
- * package {yourPackageName;}
- */
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -23,25 +19,21 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 /**
- * -------------------------------------------------------------------------------
- * |
- * | desc : Android生成适配文件工具类,帮助实现生成dimens.xml文件适配屏幕
- * |
- * |--------------------------------------------------------------------------------
- * | on 2018/9/26 created by csxiong
- * |--------------------------------------------------------------------------------
+ * @Desc : SW最小适配方案
+ * ldpi——120dpi—-1dp=0.75px
+ * mdpi—-160dpi—-1dp=1px
+ * hdpi——240dpi—-1dp=1.5px
+ * xhdpi—-320dpi—-1dp=2px
+ * xxhdpi—480dpi—-1dp=3px
+ * @Author : csxiong create on 2019/7/17
  */
-
 public class AndroidSWGenerator {
-
-    // ldpi——120dpi—-1dp=0.75px
-    // mdpi—-160dpi—-1dp=1px
-    // hdpi——240dpi—-1dp=1.5px
-    // xhdpi—-320dpi—-1dp=2px
-    // xxhdpi—480dpi—-1dp=3px
-
-    //生成最大dp内容
+    // 生成最大dp内容
     static int MAX_DP = 1000;
+    // 生成最小dp内容
+    static int MIN_DP = -1000;
+    //生成负数dimen前缀
+    static String MINUS_SHUFFIX_DES_NAME = "m_x";
     // 生成dimen前缀名称
     static String PRE_SHUFFIX_DES_NAME = "x";
     // 基准dp,以像素密度为基准比例为的是和UI标注图中px相当
@@ -50,11 +42,16 @@ public class AndroidSWGenerator {
     // 2.标注图中1px=1dp
     // 3.我们生成一套以160为基准的比例文件
     // 4.按UI标注图中PX使用我们生成的标注文件就OK了
+    //-----------------------------------------------
+    // 如果一下例子 360/160 ~ 2.25
+    // 我们生成的dp方案就要2.25px=1dp生成最小宽度文件
+    // 这样才能适应设计图比例
     static float BASE_DP = 360.0f;
     // 生成根路径,使用则修改为你想要生成的位置根路径
     static String GENATATOR_PATH = "/Users/csxiong/Desktop/Android适配基于" + ((int) BASE_DP) + "像素密度dp-px";
 
     public static void main(String[] args) {
+        //最小宽度的范围 因碎片化严重 密度越高 适配效果越好
         int[] j = {120, 160, 240, 320, 360, 420, 480, 500, 520, 560, 600};
 
         for (int m = 0; m < j.length; m++) {
@@ -69,9 +66,9 @@ public class AndroidSWGenerator {
             DocumentBuilder documentBuilder = factory.newDocumentBuilder();
             Document document = documentBuilder.newDocument();
             Element rootElement = document.createElement("resources");
-            for (int i = 0; i <= MAX_DP; i++) {
+            for (int i = MIN_DP; i <= MAX_DP; i++) {
                 Element dimen = document.createElement("dimen");
-                dimen.setAttribute("name", PRE_SHUFFIX_DES_NAME + i);
+                dimen.setAttribute("name", (i < 0 ? (MINUS_SHUFFIX_DES_NAME + (-i)) : (PRE_SHUFFIX_DES_NAME + i)));
                 dimen.setTextContent(new BigDecimal(i * (position / BASE_DP)).setScale(2, RoundingMode.HALF_UP) + "dp");
                 rootElement.appendChild(dimen);
             }
@@ -92,7 +89,6 @@ public class AndroidSWGenerator {
         } catch (ParserConfigurationException | TransformerException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
